4c015691a747b6d24a7b03b8aa5f31e81be988c1
Vanshi-ta <vanshitasuryavanshi62@gmail.com>
2024-12-15 20:29:32 +0530

    remove redundancy

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from users.models import User
 
 class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    caption = models.TextField()
     image = models.ImageField(upload_to='posts/')
-    caption = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.caption[:20]
